a0fb5f5070e2977fe5bafe67e0bff39743baab7a
tnrdd <41868860+tnrdd@users.noreply.github.com>
2020-09-10 14:36:48 +0200

    Update cycles_count.h

--- user_space/cycles_count.h
+++ user_space/cycles_count.h
@@ -21,47 +21,4 @@ struct statistics {
         uint64_t var;
 };
 
-static inline void count_cycles(uint64_t *cycles)
-{ 	
-	uint32_t i;
-	uint64_t start_low;
-	uint64_t start_high;
-	uint64_t end_low;
-	uint64_t end_high;
-
-	asm volatile ("rdtscp\n\t"
-	     	      "lfence\n\t"		
-		      : "=d" (start_high), "=a" (start_low)
-		      :
-		      : "%rbx", "%rcx");
-
-	asm volatile ("rdtscp\n\t"
-		      "lfence\n\t"
-		      : "=d" (end_high), "=a" (end_low)
-		      :
-		      : "%rbx", "%rcx");
-	
-	for (i = 0; i < REPETITION_COUNT; i++) {        
-		asm volatile ("rdtscp\n\t"
-			      "lfence\n\t"
-			      : "=d" (start_high), "=a" (start_low)
-			      :
-		              : "%rbx", "%rcx");
-
-	/*
-         **************************************************
-         *Function to benchmark here
-         **************************************************
-         */
-
-		asm volatile ("rdtscp\n\t"
-		  	      "lfence\n\t"		
-			      : "=d" (end_high), "=a" (end_low)
-			      :
-			      : "%rbx", "%rcx");
-		
-		cycles[i] = ((end_high - start_high) << LOWER_HALF_BITS) + (end_low  - start_low); 
-	}
-}
-
 #endif /* CYCLES_COUNT_H */
